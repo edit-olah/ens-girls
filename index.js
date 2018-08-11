5384eaa47d2e7cedbcc35bb6e854d11ba30b62eb
Edit Olah <edit.olah.01@gmail.com>
2018-08-11 16:02:56 +0100

    Building the contract.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var Web3 = require('web3');
+var TruffleContract = require('truffle-contract');
+var ENS = require('ethereum-ens');
+var ENSArtifacts = require('./build/contracts/ENSRegistry.json');
+var ENSContract = TruffleContract(ENSArtifacts);
+var provider = new Web3.providers.HttpProvider('http://localhost:8545');
+var ens = new ENS(provider, '0x20b3c96eb06663ed0dc3d10747707d16bce358de');
+var address = ens.resolver('ethlab.eth').addr().then(function(addr) { console.log(addr)});
+
 const express = require('express')
 const app = express()
 const path = require('path')
