b667bd4c899e9601adef30bcbf21a3d844f54e15
nhimkova <nhim0406@gmail.com>
2018-08-12 11:17:54 +0100

    modify

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ var ENSArtifacts = require('../build/contracts/ENSRegistry.json');
 var ENSContract = TruffleContract(ENSArtifacts);
 var web3 = new Web3();
 
-var provider = new Web3.providers.HttpProvider('http://localhost:8545');
-web3.setProvider(provider);
-ENSContract.setProvider(provider, "0x94f68ee4e353903f871b5c3c1346e922ae5cd357");
+// var provider = new Web3.providers.HttpProvider('http://localhost:8545');
+// web3.setProvider(provider);
+// ENSContract.setProvider(provider, "0x94f68ee4e353903f871b5c3c1346e922ae5cd357");
 
-console.log(web3.eth.accounts[0])
-var ens = new ENS(provider, "0x94f68ee4e353903f871b5c3c1346e922ae5cd357");
+// console.log(web3.eth.accounts[0])
+// var ens = new ENS(provider, "0x94f68ee4e353903f871b5c3c1346e922ae5cd357");
 //console.log(ENSContract.address)
 
 // var address = ens.resolver('ethlab.eth').addr().then(function(addr) { 
